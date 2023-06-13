fa71f48a97c99fa89cdf812a77861618d7a20827
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2023-06-13 19:45:55 +0300

    commit

---
+++ udemyourse/ornek1.py
@@ -0,0 +1,18 @@
+print("merhaba \a ")
+
+veri = chr(65)
+
+print(type(veri))
+
+print(veri,15,True,sep= " * ")
+
+# pythonda str + int olmaz
+
+print(1_000_000)
+
+print(type(True))
+print(type(5j))
+print(3e4)
+print(6 / 3 )
+print(6 // 3 )
+print(6 **3 ) # ** kuvvet alma demek
